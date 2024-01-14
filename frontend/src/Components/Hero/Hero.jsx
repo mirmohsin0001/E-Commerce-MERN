9c9bcda2e7732b7501a4ddf1f2c9ad47a420fe8e
mirmohsin0001 <mirmohsin0001@gmail.com>
2024-01-14 20:19:06 +0530

    changed images

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -7,6 +7,7 @@ import hero_image from "../Assets/hero_image.png";
 const Hero = () => {
   return (
     <div className="hero">
+
       <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
         <div>
@@ -25,9 +26,11 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      
       <div className="hero-right">
         <img src={hero_image} alt="" />
       </div>
+
     </div>
   );
 };
